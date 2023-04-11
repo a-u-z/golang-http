d77c6d0f73faccda2249445ab1d1d760440c9259
a-u-z <qinemperor0206@gmail.com>
2023-04-11 10:59:46 +0800

    fix/get-method

--- get.go
+++ get.go
@@ -2,23 +2,40 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
 )
 
 func (r *Req) get(path string) (respString string, err error) {
-	resp, err := http.Get(r.baseUrl)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
 
-	respByte, err := io.ReadAll(resp.Body)
+	client := &http.Client{}
+
+	req, err := http.NewRequest(http.MethodGet, r.baseUrl+path, nil)
 	if err != nil {
 		fp, n, l := getFilePathFuncNameAndLine()
 		err = fmt.Errorf(fp, n, l, err)
 		return
 	}
 
-	return string(respByte), nil
+	// 設置 HTTP 標頭
+	for _, v := range r.header {
+		req.Header.Add(v.key, fmt.Sprintf("%v", v.value))
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fp, n, l := getFilePathFuncNameAndLine()
+		err = fmt.Errorf(fp, n, l, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fp, n, l := getFilePathFuncNameAndLine()
+		err = fmt.Errorf(fp, n, l, err)
+		return
+	}
+
+	return string(body), nil
 }
